202231431ca27a4770225010fa41797bfa4ef402
MacBooK Air <macbookair@seaafdg.home>
2022-03-14 18:01:50 +0000

    activity summary table style change

--- resources/views/activity_summary.blade.php
+++ resources/views/activity_summary.blade.php
@@ -7,46 +7,55 @@
     <title>Activity Report | {{env('APP_NAME')}}</title>
 </head>
 <body>
-    <table>
-        <thead>
+    <table style="border-spacing: 0; border-top: 1px solid black; border-right: 1px solid black" width="100%">
+        <thead style="font-weight: bolder">
             <tr>
-                <td>N°</td>
-                <td>ACTIVITIES</td>
-                <td>Note</td>
-                <td>Status</td>
-                <td>Deadline for completion of activity</td>
-                <td>Budget</td>
-                <td>Actual spent</td>
+                <td style="border-bottom: 1px solid black; border-left: 1px solid black">N°</td>
+                <td style="border-bottom: 1px solid black; border-left: 1px solid black">ACTIVITIES</td>
+                <td style="border-bottom: 1px solid black; border-left: 1px solid black">Note</td>
+                <td style="border-bottom: 1px solid black; border-left: 1px solid black">Status</td>
+                <td style="border-bottom: 1px solid black; border-left: 1px solid black">Deadline for completion of activity</td>
+                <td style="border-bottom: 1px solid black; border-left: 1px solid black">Budget</td>
+                <td style="border-bottom: 1px solid black; border-left: 1px solid black">Actual spent</td>
             </tr>
         </thead>
         <tbody>
+            @php
+                $num = 0;
+            @endphp
             @for($i=1; $i <= count($activities); $i++)
+                @php
+                    $num += 1;
+                @endphp
                 @if ($activities[$i - 1]->outcome_id)
                     <tr>
-                        <td style="color: lime; font-weight: bolder" colspan="6">
+                        <td style="border-bottom: 1px solid black; border-left: 1px solid black; color: lime; font-weight: bolder" colspan="7">
                             {{$activities[$i -1]->outcome->name ?? ""}}
                         </td>
                     </tr>
                 @endif
                 <tr>
-                    <td>{{$i ?? ""}}</td>
-                    <td>{{$activities[$i - 1]->name ?? ""}}</td>
-                    <td>{{$activities[$i - 1]->description ?? ""}}</td>
-                    <td>{{$activities[$i - 1]->status ?? ""}}</td>
-                    <td>{{$activities[$i - 1]->end_date ?? ""}}</td>
-                    <td>{{$activities[$i - 1]->budget ?? ""}}</td>
-                    <td>{{$activities[$i - 1]->amount_spent ?? ""}}</td>
+                    <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$num ?? ""}}</td>
+                    <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$activities[$i - 1]->name ?? ""}}</td>
+                    <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$activities[$i - 1]->description ?? ""}}</td>
+                    <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$activities[$i - 1]->status ?? ""}}</td>
+                    <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$activities[$i - 1]->end_date ?? ""}}</td>
+                    <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$activities[$i - 1]->budget ?? ""}}</td>
+                    <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$activities[$i - 1]->amount_spent ?? ""}}</td>
                 </tr>
                 @if ($activities[$i - 1]['children'] && count($activities[$i - 1]['children']) > 0 )
                     @for ($j = 0; $j < count($activities[$i - 1]['children']); $j++)
+                        @php
+                            $num += 1;
+                        @endphp
                     <tr>
-                        <td>{{$i ?? ""}}</td>
-                        <td>{{$activities[$i - 1]['children'][$j]->name ?? ""}}</td>
-                        <td>{{$activities[$i - 1]['children'][$j]->description ?? ""}}</td>
-                        <td>{{$activities[$i - 1]['children'][$j]->status ?? ""}}</td>
-                        <td>{{$activities[$i - 1]['children'][$j]->end_date ?? ""}}</td>
-                        <td>{{$activities[$i - 1]['children'][$j]->budget ?? ""}}</td>
-                        <td>{{$activities[$i - 1]['children'][$j]->amount_spent ?? ""}}</td>
+                        <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$num ?? ""}}</td>
+                        <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$activities[$i - 1]['children'][$j]->name ?? ""}}</td>
+                        <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$activities[$i - 1]['children'][$j]->description ?? ""}}</td>
+                        <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$activities[$i - 1]['children'][$j]->status ?? ""}}</td>
+                        <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$activities[$i - 1]['children'][$j]->end_date ?? ""}}</td>
+                        <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$activities[$i - 1]['children'][$j]->budget ?? ""}}</td>
+                        <td style="border-bottom: 1px solid black; border-left: 1px solid black">{{$activities[$i - 1]['children'][$j]->amount_spent ?? ""}}</td>
                     </tr>
                     @endfor
                     
